48b284054dd6e0034bddf20e2cad38739a5417c6
Anshuman Tewary <150276075+anshumantewary-pm@users.noreply.github.com>
2025-07-04 01:13:29 +0530

    Update My First Program.py

--- My First Program.py
+++ My First Program.py
@@ -1,3 +1,8 @@
 print("hello World")
 print("So I am loving till now\nbut i dont't know what will happen next")
 #Wow I can use or comment here !
+''' 
+I just wanted to create numereous line
+well it seems fun
+Oops ur still here 
+'''
