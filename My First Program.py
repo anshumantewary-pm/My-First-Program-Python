df80124dcbc2729a1b5a638bf1a997b242b12639
Anshuman Tewary <150276075+anshumantewary-pm@users.noreply.github.com>
2025-07-04 01:52:16 +0530

    Update My First Program.py

--- My First Program.py
+++ My First Program.py
@@ -6,3 +6,18 @@ I just wanted to create numereous line
 well it seems fun
 Oops ur still here 
 '''
+print("hello World my name is \"Anshuman Tewary\"")
+a=24
+print(a)
+b="Hello"
+print(b)
+c=34
+print(a+c)
+a=12.23
+b=-1
+c="Hi World"
+d=None
+print("Printing the data type", type(a))
+print("Printing the data type", type(b))
+print("Printing the data type", type(c))
+print("Printing the data type", type(d))
